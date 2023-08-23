6da4254d0b58fefd3fa4aa1201f193c2b6468ff8
Jovany Vergara <diego.jobany11@gmail.com>
2023-08-23 01:07:17 -0500

    [] creating file to script for get curp from pdf

---
+++ getCurpFromDocument.groovy
@@ -0,0 +1,8 @@
+// https://mvnrepository.com/artifact/net.sourceforge.tess4j/tess4j
+@Grapes(
+    @Grab(group='net.sourceforge.tess4j', module='tess4j', version='5.8.0')
+)
+import net.sourceforge.tess4j.Tesseract;
+
+hello = "Hola"
+println hello
